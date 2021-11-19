784b7e20aa06b08077444a2f5b3928012551ecc1
Alicefortes <alicefortsp@gmail.com>
2021-11-19 20:41:43 -0300

    Ajuste pag carrinho + pag produto unico

--- front/src/index.js
+++ front/src/index.js
@@ -8,13 +8,14 @@ import Topbar from './components/topbar';
 import App from './App';
 import Cart from './views/cart';
 import ClientDashboard from './views/clientDashboard';
-
+import ProdutoUnico from './views/produtoUnico';
 ReactDOM.render(
   <Router>
   <Routes>
       <Route path='/' exact={true} element={<App/>}/>
       <Route path='/carrinho' element={<Cart/>}/>
       <Route path='/usuario' element={<ClientDashboard/>}/>
+      <Route path='/produto' element={<ProdutoUnico/>}/>
 
   </Routes>
   <Topbar/>
