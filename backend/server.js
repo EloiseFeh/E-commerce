6a05aab9b3dcabc0fac74455738eadc335bc13be
CaioBrandz <caiobrandz@gmail.com>
2022-01-01 05:10:59 -0300

    att rotas server

--- backend/server.js
+++ backend/server.js
@@ -10,5 +10,15 @@ app.use(express.json());
 app.use("/auth", require("./routes/jwtAuth"));
 app.use("/dashboard", require("./routes/dashboard"));
 
+//operacao de alteracao de dados do usuario, seja ele adm ou nao
+app.use("/perfil", require("./routes/perfil"));
+
+//visualizacao de produtos
+app.use("/produtos", require("./routes/produtosVisual"));
+
+//operacoes adm
+//app.use("/admProdutos", require("./routes/admProdutos"));
+//app.use("/admCategorias", require("./routes/admCategorias"));
+
 app.listen(process.env.PORT || 5000);
 console.log("server on!");
