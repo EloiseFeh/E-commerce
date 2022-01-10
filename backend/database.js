c1c19f8ae2be6f7d0dc297a3db5bf7c63874ba1e
Eloise Ferreira <eloiseferreira045@gmail.com>
2022-01-10 19:07:26 -0300

    começando listagem na home

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  password: "0203lili",
+  password: "bd123",
   host: "localhost",
   port: "5432",
-  database: "e-livraria",
+  database: "elivraria",
 });
 
 module.exports = pool;
--- front/src/views/home.js
+++ front/src/views/home.js
@@ -1,28 +1,37 @@
-import {React, useState} from "react";
-import CarouselHome from "../components/carousel"
-import Banner from "../components/banner"
+import { React, useState, useEffect } from "react";
+import CarouselHome from "../components/carousel";
+import Banner from "../components/banner";
 import Categoria from "../components/categoria";
 import Livros from "../components/livros";
 import SliderLivros from "../components/sliderlivros";
 import { Container } from "react-bootstrap";
 
-export default function Home(){
-    const [livros] = useState([
-        { id:'1', title: 'Come back', autor:'Raquel Q' , preco:'R$ 24,50'},
-        { id:'2', title: 'Nunca fui', autor:'Machado A', preco:'R$ 19,90'},
-        { id:'3', title: 'Alcohol free', autor:'Monteiro L', preco:'R$ 35,50'},
-        { id:'4', title: 'Your eyes', autor:'Clarice L', preco:'R$ 32,90'},
-        { id:'5', title: 'Oasis poty', autor:'Graciliano', preco:'R$ 24,90'},
-        { id:'6', title: 'Love shot', autor:'José Alencar', preco:'R$ 19,00'},
-        { id:'7', title: 'Sad Girl', autor:'Guimaraes', preco:'R$ 14,50'},
-    ]);
+export default function Home() {
+  const [livros, setLivros] = useState([]);
 
-    return(
-        <> 
-            <CarouselHome/>
-            <Banner/>
-            <Categoria/>
-            <Container className="mt-5">
+  const getLivros = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/produtosVisual/");
+      const jsonData = await response.json();
+
+      setLivros(jsonData);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getLivros();
+  }, []);
+
+  console.log(livros);
+
+  return (
+    <>
+      <CarouselHome />
+      <Banner />
+      <Categoria />
+      {/* <Container className="mt-5">
                 <div>
                     <h2>Destaques</h2>
                 </div>
@@ -33,8 +42,8 @@ export default function Home(){
                     <h2>Novidade</h2>
                 </div>
                 <SliderLivros sliderlivros={livros}/>
-            </Container>
-            <Livros livros={livros}/>
-        </>
-    ) 
+            </Container> */}
+      <Livros livros={livros} />
+    </>
+  );
 }
