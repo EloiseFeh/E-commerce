19053f5669d657351a132e3dfed4dcf6d7814f29
CaioBrandz <caiobrandz@gmail.com>
2022-02-04 17:51:59 -0300

    routes relatorios do adm

--- backend/routes/admVendas.js
+++ backend/routes/admVendas.js
@@ -77,5 +77,70 @@ router.delete("/cliente/excluirVenda/:id", async(req,res) => {
     }
 });
 
+//////////////////////////////////////////////////////////RELATORIOS/////////////////////////////////////
+
+router.post("/relatorioClientes", async (req, res) => {
+    try {
+
+        //pegando as datas
+        const {data_inicio,data_fim} = req.body;
+        
+        //query
+        const VendasTodosCliente = await pool.query(
+        "select v.id_usuario, u.nome, count(v.id_usuario) as vezes from venda as v, usuario as u "+
+        "where v.id_usuario = u.id and data_hora between $1::date and $2::date + time '23:59:59'" +
+        "group by v.id_usuario, u.nome order by vezes DESC",
+        [data_inicio,data_fim]
+      );
+
+        res.json(VendasTodosCliente.rows);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error!");
+    }
+});
+
+
+router.get("/relatorioEstoque0", async (req, res) => {
+    try {
+        
+        //query para livros que tem quantidade menor que 1
+        const estoqueNo0 = await pool.query(
+        "select id,descricao,preco,quantidade from livro where quantidade <1 order by descricao asc",
+        []
+      );
+      
+        res.json(estoqueNo0.rows);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error!");
+    }
+});
+
+
+router.post("/relatorioPorDia", async (req, res) => {
+    try {
+
+        //pegando as datas
+        const {data_inicio,data_fim} = req.body;
+        
+        //query vendas por dia
+        const VendasPorDia = await pool.query(
+        "select v.data_hora::date, sum(l.preco * vl.quantidade) as total "+
+        "from venda as v, livro as l, venda_livro as vl where v.id = vl.id_venda and vl.id_livro = l.id "+
+        "and data_hora between $1::date and $2::date + time '23:59:59' " +
+        "group by v.data_hora::date order by data_hora asc",
+        [data_inicio,data_fim]
+      );
+
+        res.json(VendasPorDia.rows);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error!");
+    }
+});
 
 module.exports = router;
